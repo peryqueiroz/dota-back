c002b0e2515d0d183581ab2e83fb426f2fbcc5e4
Pery Vieira <inv.pvieira@ifood3rd.com>
2023-05-12 15:38:36 -0300

    first

---
+++ src/main/java/com/b1house/dotaslz/DotaslzApplication.java
@@ -0,0 +1,13 @@
+package com.b1house.dotaslz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaslzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaslzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b1house/dotaslz/DotaslzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1house.dotaslz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaslzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
