fb28c92dc08dd48fb5676d1a782291b73ef3972d
Pery Vieira <inv.pvieira@ifood3rd.com>
2023-06-03 11:40:11 -0300

    fix

--- gitlab-deploy/.gitlab-deploy.prod.sh
+++ gitlab-deploy/.gitlab-deploy.prod.sh
@@ -7,7 +7,7 @@ string=$DEPLOY_SERVER_IP
 array=(${string//,/ })
 # Iterate servers for deploy and pull last commit
 # Careful with the ; https://stackoverflow.com/a/20666248/1057052
-for i in “${!array[@]}”; do
-  echo “Deploy project on server ${array[i]}”
-ssh ubuntu@${array[i]} “cd ./Staging/vr && git stash && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo yarn install && sudo npm run staging”
+for i in '${!array[@]}'; do
+  echo 'Deploy project on server ${array[i]}'
+ssh ubuntu@${array[i]} 'cd ./Staging/vr && git stash && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo yarn install && sudo npm run staging'
 done
